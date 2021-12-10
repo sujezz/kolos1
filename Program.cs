105c40dc02f2e5e7c4f660f9cc7612d4fe4cbb98
Jan Slomian <sujezz@gmail.com>
2021-12-10 20:43:38 +0100

    fix

--- Program.cs
+++ Program.cs
@@ -6,17 +6,12 @@ namespace Kolos1
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
             bool exit = false;
             while (exit == false)
             {
-            
-                Console.WriteLine("Wybierz opcję");
-                Console.WriteLine("1. Sortowanie liczb");
-                Console.WriteLine("2. Sprawdzanie email");
-                Console.WriteLine("3. Rysowanie prostokąta znakiem");
-                Console.WriteLine("4. Wyjście");
+                Menu();
                 ConsoleKeyInfo KP = Console.ReadKey();
                 switch (KP.Key)
                 {
@@ -24,83 +19,101 @@ namespace Kolos1
                         exit = true;
                         break;
                     case ConsoleKey.D1:
-                        // zad 1
-                        // Console.Clear();
-                        Console.WriteLine("Zadanie 1:");
-                        Console.WriteLine("Podaj 3 liczby");
-                        string[] liczba = new string[3];
-                        for (int i = 0; i < 3; i++)
-                        {
-                            liczba[i] = Console.ReadLine();
-                            while (!int.TryParse(liczba[i], out int si))
-                            {
-
-                                Console.WriteLine("To nie jest liczba");
-                                liczba[i] = Console.ReadLine();
-                            }
-                        }
-
-                        Array.Sort(liczba);
-                        foreach (string i in liczba)
-                        {
-                            Console.WriteLine(i);
-                        }
+                        Zad1();
                         break;
 
                     case ConsoleKey.D2:
-                        // zad 2
-                        Console.Clear();
-                        Console.WriteLine("Zadanie 2:");
-                        Console.WriteLine("Podaj Email");
-                        string email = Console.ReadLine();
-                        var reg = @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z";
-                        if (!Regex.IsMatch(email, reg, RegexOptions.IgnoreCase))
-                        {
-                            // if (email.Count(f => f == '@') != 1 || !email.Contains('.') || email[0] != '@') można się bawić...
-
-                            Console.WriteLine("Nieprawidłowy email");
-                        }
-                        else
-                        {
-                            string[] dane = email.Split('@', 2);
-                            foreach (string d in dane) // alternatywnie po prostu wypisac dane[0] i dane [1]
-                            {
-                                Console.WriteLine(d.ToUpper());
-                            }
-                        }
+                        Zad2();
                         break;
 
                     case ConsoleKey.D3:
-                        // zad 3
                         Console.Clear();
-                        bool jestok = false;
-                        do
-                        {
-                            Console.WriteLine("Zadanie 3:");
-                            string szerokosc, wysokosc, znak;
-                            Console.WriteLine("Podaj szerokość");
-                            szerokosc = Console.ReadLine();
-                            Console.WriteLine("Podaj wysokość");
-                            wysokosc = Console.ReadLine();
-                            Console.WriteLine("Podaj znak");
-                            znak = Console.ReadLine();
-                            if (int.TryParse(szerokosc, out int x) && int.TryParse(wysokosc, out int y) && znak.Length > 0)
-                            {
-                                for (int i = 0; i < y; i++)
-                                {
-                                    for (int j = 0; j < x; j++)
-                                        Console.Write(znak[0]); // bierze tylko pierwszy wpisany znak
-                                    Console.WriteLine();
-
-                                }
-                                jestok = true;
-                            }
-                            else Console.WriteLine("Szerokość i wysokość muszą być liczbami a znak nie może być pusty");
-                        } while (jestok == false);
+                        Zad3();
                         break;
                     default: Console.WriteLine("Nie ma takiej opcji"); break;
                 }
             }
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("Wybierz opcję");
+            Console.WriteLine("1. Sortowanie liczb");
+            Console.WriteLine("2. Sprawdzanie email");
+            Console.WriteLine("3. Rysowanie prostokąta znakiem");
+            Console.WriteLine("4. Wyjście");
+        }
+        public static void Zad1()
+        {
+            Console.Clear();
+            Console.WriteLine("Zadanie 1:");
+            Console.WriteLine("Podaj 3 liczby");
+            string[] liczba = new string[3];
+            for (int i = 0; i < 3; i++)
+            {
+                liczba[i] = Console.ReadLine();
+                while (!int.TryParse(liczba[i], out int si))
+                {
+                    Console.WriteLine("To nie jest liczba");
+                    liczba[i] = Console.ReadLine();
+                }
+            }
+
+            Array.Sort(liczba);
+            foreach (string i in liczba)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static void Zad2()
+        {
+            Console.Clear();
+            Console.WriteLine("Zadanie 2:");
+            Console.WriteLine("Podaj Email");
+            string email = Console.ReadLine();
+            var reg = @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z";
+            if (!Regex.IsMatch(email, reg, RegexOptions.IgnoreCase))
+            {
+                // if (email.Count(f => f == '@') != 1 || !email.Contains('.') || email[0] != '@') można się bawić...
+
+                Console.WriteLine("Nieprawidłowy email");
+            }
+            else
+            {
+                string[] dane = email.Split('@', 2);
+                foreach (string d in dane) // alternatywnie po prostu wypisac dane[0] i dane [1]
+                {
+                    Console.WriteLine(d.ToUpper());
+                }
+            }
+        }
+        public static void Zad3()
+        {
+            Console.Clear();
+            bool jestok = false;
+            do
+            {
+                Console.WriteLine("Zadanie 3:");
+                string szerokosc, wysokosc, znak;
+                Console.WriteLine("Podaj szerokość");
+                szerokosc = Console.ReadLine();
+                Console.WriteLine("Podaj wysokość");
+                wysokosc = Console.ReadLine();
+                Console.WriteLine("Podaj znak");
+                znak = Console.ReadLine();
+                if (int.TryParse(szerokosc, out int x) && int.TryParse(wysokosc, out int y) && znak.Length > 0)
+                {
+                    for (int i = 0; i < y; i++)
+                    {
+                        for (int j = 0; j < x; j++)
+                            Console.Write(znak[0]); // bierze tylko pierwszy wpisany znak
+                        Console.WriteLine();
+
+                    }
+                    jestok = true;
+                }
+                else Console.WriteLine("Szerokość i wysokość muszą być liczbami a znak nie może być pusty");
+            } while (jestok == false);
         }
     }
 }
